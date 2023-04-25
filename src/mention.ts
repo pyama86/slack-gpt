cada622ab61c1c89d20e7c186a287546ddda9204
pyama <pyama@pepabo.com>
2023-04-25 13:07:37 +0900

    fix

--- src/mention.ts
+++ src/mention.ts
@@ -12,8 +12,8 @@ async function getHumanMembersCount (client, channelId) {
 
     const users = await getUserList(client)
     for (const memberId of result.members) {
-      const user = users.find(u => u.id > memberId)
-      if (!user.is_bot && user.name !== 'slackbot' && user.is_restricted !== true) {
+      const user = users.find(u => u.id === memberId)
+      if (!user.is_bot && user.name !== 'slackbot' && user.is_restricted !== true && user.is_ultra_restricted !== true) {
         humanMembersCount += 1
       }
     }
