41d78a97066cf8f4efd30b04ed3273ee4af6fd57
pyama <pyama@pepabo.com>
2023-05-17 23:35:01 +0900

    shapeup

--- src/mention.ts
+++ src/mention.ts
@@ -1,31 +1,6 @@
 import { ask, getUserList } from './utils'
 import { ChatCompletionRequestMessageRoleEnum } from 'openai'
 
-async function getHumanMembersCount (client, channelId) {
-  try {
-    const members = await client.conversations.members({
-      channel: channelId,
-      liimt: 200
-    })
-
-    let humanMembersCount = 0
-
-    const users = await getUserList(client)
-    for (const memberId of members.members) {
-      const user = users.find(u => u.id === memberId)
-      if (('is_bot' in user && !user.is_bot) && user.name !== 'slackbot' && user.is_restricted !== true && user.is_ultra_restricted !== true) {
-        humanMembersCount += 1
-      }
-    }
-
-    console.log('human count in ' + channelId + ':' + humanMembersCount)
-    return humanMembersCount
-  } catch (error) {
-    console.error(`Error: ${error}`)
-    return 0
-  }
-}
-
 export const appMention: any = async ({ event, client, say }) => {
   const channelId = event.channel
   const botUserId = process.env.BOT_USER_ID
@@ -34,35 +9,6 @@ export const appMention: any = async ({ event, client, say }) => {
       channel: channelId,
       ts: event.thread_ts || event.ts
     })
-
-    const channel_member_count = await getHumanMembersCount(client, channelId)
-    let channelInfo = null
-    try {
-      channelInfo = await client.conversations.info({
-        channel: channelId
-      })
-    } catch (error) {
-      console.error(`Error: ${error}`)
-    }
-
-    if (
-      (
-        channel_member_count > 0 &&
-        channel_member_count < 10
-      ) ||
-      (
-        channelInfo != null &&
-        channelInfo.channel.name && (
-          channelInfo.channel.name.includes('times') ||
-          channelInfo.channel.name.includes('test')
-        )
-      )
-    ) {
-      say({
-        text: 'このチャンネルでの利用は非推奨のため、そろそろ嘘を混ぜます',
-        thread_ts: event.ts
-      })
-    }
 
     if (!replies.messages) {
       await say(
